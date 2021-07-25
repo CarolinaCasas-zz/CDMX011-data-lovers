7d97dc22ad1d324b32f4981ccf14375a586a3708
CarolinaCasas <casasc9@uabc.edu.mx>
2021-07-24 22:06:29 -0700

    ya logramos filtrar

--- src/main.js
+++ src/main.js
@@ -3,20 +3,25 @@ import manipulacionData from './data.js';
 import info from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
+/*document.getElementById("tiposPokemon").addEventListener("change", function () {
+  const elementoSeleccionado = document.getElementById("tiposPokemon").value;
+  let listaPokeTipo = info.pokemon.filter(poke =>elementoSeleccionado===poke.type);
+  
+  
+    console.log(listaPokeTipo);
+}*/
 
 document.getElementById("tiposPokemon").addEventListener("change",function(){
- recuperarSeleccionado();  
-  });
-  
-function recuperarSeleccionado(){
- const elementoSeleccionado= document.getElementById("tiposPokemon").value;
+  recuperarSeleccionado();  
+   });
+   
+ function recuperarSeleccionado(){
+  const elementoSeleccionado= document.getElementById("tiposPokemon").value;
+  const infoPokemon= info.pokemon;
 
-  info.pokemon.forEach(poke=>{
-   const tipo= poke.type;
-  
-   if(elementoSeleccionado===tipo){
-    alert("El elemento seleccionado es"+elementoSeleccionado);
-  } else{ alert("esto no esta jalando");
-  }
-  })
+  const listaPokeTipo = infoPokemon.filter(poke =>elementoSeleccionado==poke.type);
+
+
+  console.log(listaPokeTipo);
 }
+
