c2f8320e0ab69791eac5472aaa65350258577d5e
CarolinaCasas <casasc9@uabc.edu.mx>
2021-07-27 11:38:43 -0700

    imprimeimagenes

--- src/data.js
+++ src/data.js
@@ -2,25 +2,6 @@
 const manipulacionData ={
 
 
-/*const imagenes= ()=>{
-  document.getElementById("img1").src="image2.jpg";
-}*/
-
-/*function cambiarImagenJS(){
-  for (let = 0; < array.length;++) {
-    const element = array[
-    ];
-    
-  }
-}*/
-  
-/*export const example = () => {
-  return 'example';
-};
-
-export const anotherExample = () => {
-  return 'OMG';
-};*/
 
 
 }
--- src/main.js
+++ src/main.js
@@ -3,25 +3,41 @@ import manipulacionData from './data.js';
 import info from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-/*document.getElementById("tiposPokemon").addEventListener("change", function () {
-  const elementoSeleccionado = document.getElementById("tiposPokemon").value;
-  let listaPokeTipo = info.pokemon.filter(poke =>elementoSeleccionado===poke.type);
-  
-  
-    console.log(listaPokeTipo);
-}*/
 
 document.getElementById("tiposPokemon").addEventListener("change",function(){
   recuperarSeleccionado();  
    });
    
- function recuperarSeleccionado(){
-  const elementoSeleccionado= document.getElementById("tiposPokemon").value;
-  const infoPokemon= info.pokemon;
+   function recuperarSeleccionado(){
+    const elementoSeleccionado= document.getElementById("tiposPokemon").value;
+    const infoPokemon= info.pokemon;
+  
+    const pokeTipoSeleccionado = infoPokemon.filter(poke =>elementoSeleccionado==poke.type);
+  
+    const imgTipoSelect = pokeTipoSeleccionado.map(imagenes => imagenes.img);
 
-  const listaPokeTipo = infoPokemon.filter(poke =>elementoSeleccionado==poke.type);
+    imgTipoSelect.forEach(imgSelect=> {
+      const contenedorImg = document.getElementById("imagenes");
+      const elementoImg= document.createElement("img"); 
+      elementoImg.src=imgSelect;
 
-
-  console.log(listaPokeTipo);
+      contenedorImg.appendChild(elementoImg);
+    })
+    
+  
+  
 }
-
+    
+    
+    /*var linew= document.createElement("img");    
+        var contenido = document.attr("src", dataImg );
+        sugerenciasImg.appendChild(linew);
+        linew.appendChild(contenido);
+            
+        });
+  
+        const sugerenciasImg =document.getElementById("sugerencias");
+        
+        console.log(imgSelect*/  
+  
+  
